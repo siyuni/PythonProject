ea9fd7a4ddaab7c07e0980db7d8d5e9848e05afa
kshiy <itsc@google.com>
2023-02-10 21:49:20 +0900

    Day06

---
+++ Day06/Section12/Ex12-4-module4.py
@@ -0,0 +1,11 @@
+'''
+별명 사용하기
+'''
+import converter as cvt
+
+miles = kilometer_to_miles(150)
+print('150km={}miles'.format(miles))
+
+
+pounds = gram_to_pounds(1000)
+print('1000g={}pounds'.format(pounds))
